a5687589a407f81f14dbcfcb346e172c2ec2e51f
hans.latt <hans.latt@tptlive.ee>
2022-06-15 18:01:43 +0300

    All files

---
+++ kta21eBlog-main/resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('title', 'Welcome')
+
+@section('content')
+    <h1>Welcome</h1>
+    <p1>Explore around this blog and choose between links listed above.</p1>
+@endsection
